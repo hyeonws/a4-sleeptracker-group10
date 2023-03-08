3c7ee536b2151f0ecfb6d1096420e447cdc75439
orvielalz <orvielalz@gmail.com>
2023-03-07 23:58:23 -0800

    accessed the time from overnight data

--- sleeptracker/src/app/home/home.page.ts
+++ sleeptracker/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SleepService } from '../services/sleep.service';
 import { SleepData } from '../data/sleep-data';
 import { OvernightSleepData } from '../data/overnight-sleep-data';
@@ -10,12 +10,18 @@ import { now } from '@ionic/core/dist/types/utils/helpers';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 	daynight:string;
 	greetings:Array<string> = ["Good morning.", "Good afternoon.", "Good night."];
 	messages:Array<string> = ["Hope you got a good night's rest.", "How's your day going?", "Go get some well-earned rest."]
 	greeting:string;
 	message:string;
+	loggedDate:string;
+
+
+	startSleep:string=new Date().toISOString();
+	endSleep:string=new Date().toISOString();
+	overnightSleepDataArray: OvernightSleepData[] = [];
 	
   	constructor(public sleepService:SleepService) {
 		let now = new Date();
@@ -32,18 +38,28 @@ export class HomePage {
 			this.greeting = this.greetings[2];
 			this.message = this.messages[2];
 		}
+		this.loggedDate = this.allSleepData[0]['loggedAt'].toDateString();
 	}
 
-	ngOnInit() {
+	ngOnInit() {;
+		console.log(this.loggedDate);
 		console.log(this.allSleepData);
 		console.log(new Date().getHours());
 		console.log(this.greeting);
 		console.log(this.message);
+		this.overnightSleepDataArray = SleepService.AllOvernightData;
 	}
 
 	/* Ionic doesn't allow bindings to static variables, so this getter can be used instead. */
 	get allSleepData() {
 		return SleepService.AllSleepData;
+	}
+
+	addLog() {
+		var newStartSleep = new Date(this.startSleep);
+		var newEndSleep = new Date(this.endSleep);
+		let overnightSleep: OvernightSleepData = new OvernightSleepData(newStartSleep, newEndSleep);
+		this.sleepService.logOvernightData(overnightSleep);
 	}
 
 }
