16f270fc817ac05300fd8ee27c993f8a01688002
hyeonws <hyeonws@uci.edu>
2023-03-08 23:39:05 -0800

    Sleepiness log working, cancel and confirm buttons don't close modal but confirm works, have to store them for viewing

--- sleeptracker/src/app/home/home.page.ts
+++ sleeptracker/src/app/home/home.page.ts
@@ -14,33 +14,31 @@ import { now } from '@ionic/core/dist/types/utils/helpers';
 })
 export class HomePage implements OnInit {
 	@ViewChild(IonModal) modal!:IonModal;
-	daynight:string;
 	greetings:Array<string> = ["Good morning.", "Good afternoon.", "Good night."];
 	messages:Array<string> = ["Hope you got a good night's rest.", "How's your day going?", "Go get some well-earned rest."]
 	greeting:string;
 	message:string;
 	loggedDate:string;
-	test:any;
-
-	name:string="";
-
+	sleepinessArray:Array<string> = [];
 	startSleep:Date;
 	endSleep:Date;
 	overnightSleepDataArray: OvernightSleepData[] = [];
 	sleepMsg:string = "You don't have any sleep logged for today.";
+	sleepinessMsg:string = "You don't have any sleepiness logs for today.";
+	sliderRange!:number;
 	
   	constructor(public sleepService:SleepService) {
+		for (let i = 1; i < 8; i++) {
+			this.sleepinessArray[i-1] = StanfordSleepinessData.ScaleValues[i];
+		}
 		let now = new Date();
 		if (now.getHours() > 6 && now.getHours() < 12) {
-			this.daynight = "day";
 			this.greeting = this.greetings[0];
 			this.message = this.messages[0];
 		} else if (now.getHours() < 19) {
-			this.daynight = "afternoon";
 			this.greeting = this.greetings[1];
 			this.message = this.messages[1];
 		} else {
-			this.daynight = "night";
 			this.greeting = this.greetings[2];
 			this.message = this.messages[2];
 		}
@@ -54,26 +52,23 @@ export class HomePage implements OnInit {
 		console.log(this.allSleepData);
 		this.overnightSleepDataArray = SleepService.AllOvernightData;
 		console.log(this.startSleep);
+		console.log(this.sleepinessArray);
 	}
 
 	/* Ionic doesn't allow bindings to static variables, so this getter can be used instead. */
 	get allSleepData() {
 		return SleepService.AllSleepData;
 	}
-
-	addLog() {
-		console.log("Button clicked!");
-	}
 	
 	cancel() {
-		this.modal.dismiss(null, 'cancel');
+		this.modal.dismiss('cancel');
 	  }
 	
-	confirm() {
+	logConfirm() {
 		let overnightSleep: OvernightSleepData = new OvernightSleepData(new Date(this.startSleep), new Date(this.endSleep));
 		this.sleepService.logOvernightData(overnightSleep);
 		this.sleepMsg = overnightSleep.summaryString();
-		this.modal.dismiss(this.name, 'confirm');
+		this.modal.dismiss('confirm');
 	}
 
 	updateStartTime(event: any) {
@@ -84,5 +79,17 @@ export class HomePage implements OnInit {
 	updateEndTime(event: any) {
 		const ev = event;
 		this.endSleep = ev.detail.value;
+	}
+
+	sleepConfirm() {
+		let sleepinessData:StanfordSleepinessData = new StanfordSleepinessData(this.sliderRange, new Date());
+		this.sleepinessMsg = sleepinessData.summaryString();
+		this.modal.dismiss();
+	}
+
+	updateRangeValue(event: any) {
+		const ev = event;
+		this.sliderRange = ev.detail.value;
+		console.log(this.sliderRange);
 	}
 }
